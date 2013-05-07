5d2daa1a0105d0322f37b1ec9cd73eb58386b5b5
seuck <abianchetti@yahoo.com>
2013-05-07 11:20:24 +0200

    Advert IDs are now autogenerated

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,5 +2,5 @@ class Advert < ActiveRecord::Base
   belongs_to :volume
   belongs_to :game
   belongs_to :page
-  attr_accessible :id, :volume_id, :game_id, :page_id
+  attr_accessible :volume_id, :game_id, :page_id
 end
